7f70374564d619f61614eb18881b20124e39741a
Maksim Kostromin <daggerok@gmail.com>
2018-12-17 04:12:25 +0200

    Kafka initial setup.

---
+++ kafka-app/src/main/java/com/github/daggerok/ddd/app/cqrsandeventsourcing/KafkaBrokerAppConfig.java
@@ -0,0 +1,8 @@
+package com.github.daggerok.ddd.app.cqrsandeventsourcing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@Configuration
+public class KafkaBrokerAppConfig { }
